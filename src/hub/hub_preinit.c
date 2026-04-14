65e4fb9beacee0d2ebbe9dfbfb896113264f4c49
Deepak Nayak <deepak.nayak@latticesemi.com>
2026-04-14 21:44:10 +0530

    git subrepo pull src/hub
    subrepo:
      subdir:   "src/hub"
      merged:   "3a10df5"
    upstream:
      origin:   "git@github.com:dnayak-lattice/libhub.git"
      branch:   "main"
      commit:   "3a10df5"
    git-subrepo:
      version:  "0.4.9"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "5e0f401"

--- src/hub/hub_preinit.c
+++ src/hub/hub_preinit.c
@@ -7,6 +7,7 @@
 
 #include "hub.h"
 #include "dal.h"
+#include "dal_busses.h"
 #include "hub_version.h"
 #include "hub_globals.h"
 #include "os_funcs.h"
@@ -46,6 +47,21 @@ hub_handle_t hub_preinit(struct hub_config *p_config)
 	if (p_hub_ctx == NULL) {
 		hub_pr_err("Failed to allocate memory for HUB context\n");
 		return NULL;
+	}
+
+	/* IF the dal bus is UART set its baud rate to 921600 */
+	for (uint32_t i = 0; i < p_config->num_busses; i++) {
+		struct dal_bus *p_bus = (struct dal_bus *)p_config->dal_busses[i];
+		enum hub_gard_bus_types bus_type = dal_get_bus_type((void *)p_bus);
+		if (HUB_GARD_BUS_UART == bus_type) {
+			struct dal_bus_uart_props *p_uart_props =
+				(struct dal_bus_uart_props *)&(p_bus->props.uart_props);
+			if (NULL == p_uart_props) {
+				hub_pr_err("Failed to get UART properties\n");
+				return NULL;
+			}
+			p_uart_props->baud_rate = 921600;
+		}
 	}
 
 	p_hub_ctx->p_config  = p_config;
